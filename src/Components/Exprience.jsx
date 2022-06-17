74e408298d159ebac9982b03551d66e4ab843523
arashrahimi055 <develarash@gmail.com>
2022-06-17 22:11:16 +0430

    commited

--- src/Components/Exprience.jsx
+++ src/Components/Exprience.jsx
@@ -9,15 +9,15 @@ const Experience = () => {
   return (
     <div className='experience'>
       <VerticalTimeline lineColor='#3e497a'>
-        <VerticalTimelineElement icon={<SchoolIcon/>} iconStyle={{background:"#3e497a" ,color:"#fff"}} date='2010 - 2018' className='vertical-timeline-element--education'>
+        <VerticalTimelineElement icon={<SchoolIcon/>} iconStyle={{background:"#000057" ,color:"#fff"}} date='2010 - 2018' className='vertical-timeline-element--education'>
           <h3 className='vertical-timeline-element-title'>My Random High School , Random State</h3>
 
         </VerticalTimelineElement>
-        <VerticalTimelineElement icon={<SchoolIcon/>} iconStyle={{background:"#3e497a" ,color:"#fff"}} date='2010 - 2018' className='vertical-timeline-element--education'>
+        <VerticalTimelineElement icon={<SchoolIcon/>} iconStyle={{background:"#000057" ,color:"#fff"}} date='2010 - 2018' className='vertical-timeline-element--education'>
           <h3 className='vertical-timeline-element-title'>My Random High School , Random State</h3>
 
         </VerticalTimelineElement>
-        <VerticalTimelineElement icon={<WorkIcon />} iconStyle={{background:"blue" ,color:"#fff"}} date='2010 - 2018' className='vertical-timeline-element--education'>
+        <VerticalTimelineElement icon={<WorkIcon />} iconStyle={{background:"#000057" ,color:"#fff"}} date='2010 - 2018' className='vertical-timeline-element--education'>
           <h3 className='vertical-timeline-element-title'>My Random High School , Random State</h3>
           <p>high School Diploma</p>
 
--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -1,19 +1,61 @@
 import React from 'react'
-// import {  Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "../styles/navigation.css"
 const Projects = () => {
+    let activeStyle = {
+     
+        color:"white"
+        
+      };
+    
+      let activeClassName = "red";
   return (
     <div className="navigation">
-        <div className='backgroundpic'>
-        <img src={require("../assets/back.jpg")} />
-
-        </div>
-        <div className='profilepic'>
-            {/* <img src={"../assets/i.jpg"} alt="s" /> */}
-            <img src={require("../assets/i.jpg")} />
-
-
-        </div>
+    <nav>
+      <ul className='ul-navigation'>
+        
+        <li className='li'>
+          <NavLink
+            to="Exprience"
+            style={({ isActive }) =>
+              isActive ? activeStyle : undefined
+            }
+          >
+            Exprience
+          </NavLink>
+        </li>
+     <li className='li'>
+     <NavLink
+            to="Projects"
+            style={({ isActive }) =>
+              isActive ? activeStyle : undefined
+            }
+          >
+            Projects
+          </NavLink>
+     </li>
+     <li className='li'>
+     <NavLink
+            to="Educatios"
+            style={({ isActive }) =>
+              isActive ? activeStyle : undefined
+            }
+          >
+            Educatios
+          </NavLink>
+     </li>
+     <li className='li'>
+          <NavLink
+            to="/"
+            style={({ isActive }) =>
+              isActive ? activeStyle : undefined
+            }
+          >
+            Home
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
  
     </div>
   )
